c4738a7895954041fbb5811f9da465d35de4a306
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2023-02-10 16:52:23 +0100

    lecture done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = require('../app');
 const PokemonModel = require('../models/Pokemon.model');
 const router = express.Router();
+const capitalize = require("../utils/capitalize.js")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -15,8 +16,21 @@ router.get("/pokemon", (req, res, next) => {
 
   // buscar la data en la base de datos
   PokemonModel.find()
+  .select("name") // esto es decir: solo devuelve el nombre de los pokemon
   .then((response) => {
-    console.log(response)
+    
+    // a veces el sistema pide que hagamos un clone de response antes de modificarlo
+
+    response.forEach((eachPokemon) => {
+      eachPokemon.name = capitalize(eachPokemon.name)
+    })
+    // console.log(response)
+    // const response2 = response.map((eachPokemon) => {
+    //   return {
+    //     _id: eachPokemon._id,
+    //     name: capitalize(eachPokemon.name)
+    //   }
+    // })
 
     res.render("pokemon/list.hbs", {
       allPokemons: response
@@ -31,6 +45,52 @@ router.get("/pokemon", (req, res, next) => {
 
   
 
+})
+
+// GET "/pokemon/:pokemonId" => renderizar los detalles de un solo pokemon por su id
+router.get("/pokemon/:pokemonId", (req, res, next) => {
+  // 1. hacemos hbs y probamos renderizar
+
+  // necesitamos el id
+  console.log(req.params.pokemonId)
+  const { pokemonId } = req.params
+
+  // necesitamos buscar el pokemon en la DB ya teniendo el id
+  // PokemonModel.findOne({_id: pokemonId})
+  PokemonModel.findById(pokemonId) // son metodos más optimizados (busquedas más rapidos)
+  .then((response) => {
+    console.log(response)
+
+    // pasariamos la data a la vista para renderizarla
+    
+    res.render("pokemon/details.hbs", {
+      onePokemon: response
+    })
+  })
+  .catch((err) => {
+    next(err)
+  })
+})
+
+// GET "/search" => renderizar un formulario de busqueda de pokemons
+router.get("/search", (req, res, next) => {
+
+  console.log("probando en la ruta")
+  console.log(req.query)
+  const { pokemon } = req.query
+
+  PokemonModel.findOne({name: pokemon})
+  .then((response) => {
+    console.log(response)
+
+    res.render("pokemon/search.hbs", {
+      foundPokemon: response
+    })
+
+  })
+  .catch((err) => {
+    next(err)
+  })
 })
 
 module.exports = router;
